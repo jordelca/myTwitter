fe66950f92da758faa417b5cd9f680c7100667a4
Jorge Delgado <delgado_jor@externos.gva.es>
2015-12-22 12:59:12 +0100

    Added multi environment support

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,10 +15,11 @@ class DefaultController extends Controller
     {
 
         $user = $this->getDoctrine()->getRepository("AppBundle:User")->findOneById(1);
-        var_dump($user);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'user' => $user->getName()
         ));
     }
 }
