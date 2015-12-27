d2afb2b0bba98580f6018a54b832020228a2070e
jordelca <jorge.delgado86@gmail.com>
2015-12-28 00:19:31 +0100

    loginAction override

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,9 +6,23 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\SecurityController as BaseController;
 
-class DefaultController extends Controller
+class DefaultController extends BaseController
 {
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request){
+        $securityContext = $this->container->get('security.authorization_checker');
+        if ($securityContext->isGranted('ROLE_USER')) {
+            return new RedirectResponse($this->generateUrl('timeline'));
+        }
+
+        $response = BaseController::loginAction($request);
+        return $response;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
